16afadae693c018bdf6d257510fcbb9771ccb11d
sangholim <55565835+sangholim@users.noreply.github.com>
2023-05-03 15:23:11 +0900

    refactor: ChatMessageResource.kt 수정 (#3)
    - chat-id 를 destination variable 로 추가

--- src/test/kotlin/talk/messageService/chat/ChatMessageResourceTest.kt
+++ src/test/kotlin/talk/messageService/chat/ChatMessageResourceTest.kt
@@ -18,18 +18,18 @@ class ChatMessageResourceTest(
 ) : BehaviorSpec({
     Given("chat 메세지 발신, 수신 stream 확인하기") {
 
-        fun chatStream() = rSocketRequester.route("stream.chat.message")
+        fun chatStream(id: String) = rSocketRequester.route("stream.chats.$id.message")
 
         When("chat 메세지 정상 발송한 경우") {
             Then("1개의 메세지가 정상 수신된다") {
                 val payload = ChatMessageVM("a", "b")
-                chatStream().dataWithType(
+                chatStream("a").dataWithType(
                         flow {
                             emit(payload)
                         }
                 ).retrieveFlow<Unit>().collect()
 
-                val received = chatStream().retrieveFlux<ChatMessageVM>()
+                val received = chatStream("a").retrieveFlux<ChatMessageVM>()
 
                 StepVerifier.create(received)
                         .expectNext(payload)
