6bd9d1957f55296100b733f32287d6363b936571
hoya <hybriddevslim@gmail.com>
2023-05-03 15:19:14 +0900

    refactor: ChatMessageResource.kt 수정 - chat-id 를 destination variable 로 추가

--- src/main/kotlin/talk/messageService/chatMessage/ChatMessageResource.kt
+++ src/main/kotlin/talk/messageService/chatMessage/ChatMessageResource.kt
@@ -3,6 +3,7 @@ package talk.messageService.chatMessage
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.onStart
+import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Controller
@@ -14,16 +15,16 @@ class ChatMessageResource(
     /**
      * client 로부터 메세지를 받아서 저장
      */
-    @MessageMapping(value = ["stream.chat.message"])
-    suspend fun receive(@Payload inboundMessages: Flow<ChatMessageVM>) {
+    @MessageMapping(value = ["stream.chats.{id}.message"])
+    suspend fun receive(@DestinationVariable id: String, @Payload inboundMessages: Flow<ChatMessageVM>) {
         chatMessageService.post(inboundMessages)
     }
 
     /**
      * client 로 메세지 전달
      */
-    @MessageMapping(value = ["stream.chat.message"])
-    fun send(): Flow<ChatMessageVM> = chatMessageService
+    @MessageMapping(value = ["stream.chats.{id}.message"])
+    fun send(@DestinationVariable id: String): Flow<ChatMessageVM> = chatMessageService
             .stream()
             .onStart {
                 emitAll(chatMessageService.latest())
