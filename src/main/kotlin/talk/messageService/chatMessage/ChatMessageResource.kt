591d85ce32c0f8d86db55477505a6c8e3ae547fa
sangholim <55565835+sangholim@users.noreply.github.com>
2023-05-04 17:54:41 +0900

    refactor: ChatMessageResource.kt 수정 (#8)
    - 수신 발신시 로그 추가

--- src/main/kotlin/talk/messageService/chatMessage/ChatMessageResource.kt
+++ src/main/kotlin/talk/messageService/chatMessage/ChatMessageResource.kt
@@ -3,6 +3,7 @@ package talk.messageService.chatMessage
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.onStart
+import org.slf4j.LoggerFactory
 import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
@@ -12,11 +13,14 @@ import org.springframework.stereotype.Controller
 class ChatMessageResource(
         private val chatMessageService: ChatMessageService
 ) {
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
     /**
      * client 로부터 메세지를 받아서 저장
      */
     @MessageMapping(value = ["stream.chats.{id}.message"])
     suspend fun receive(@DestinationVariable id: String, @Payload inboundMessages: Flow<ChatMessageVM>) {
+        logger.debug("receive message: $id")
         chatMessageService.post(inboundMessages)
     }
 
@@ -27,6 +31,7 @@ class ChatMessageResource(
     fun send(@DestinationVariable id: String): Flow<ChatMessageVM> = chatMessageService
             .stream()
             .onStart {
+                logger.debug("send message: $id")
                 emitAll(chatMessageService.latest())
             }
 }
