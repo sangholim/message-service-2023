262bdcd00fcc2c8e1c9db301bc47ff411d90326b
sangholim <55565835+sangholim@users.noreply.github.com>
2023-05-04 22:43:53 +0900

    refactor: ChatMessageService.kt 수정 (#9)
    - 메세지 저장시 데이터 리스트 로깅

--- src/main/kotlin/talk/messageService/chatMessage/ChatMessageService.kt
+++ src/main/kotlin/talk/messageService/chatMessage/ChatMessageService.kt
@@ -1,10 +1,13 @@
 package talk.messageService.chatMessage
 
 import kotlinx.coroutines.flow.*
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class ChatMessageService {
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
     private val storage = mutableListOf<ChatMessageVM>()
 
     val sender: MutableSharedFlow<ChatMessageVM> = MutableSharedFlow()
@@ -15,6 +18,8 @@ class ChatMessageService {
             messages
                     .let {
                         storage.addAll(it.toList())
+                    }.also {
+                        logger.debug("messages: $storage")
                     }
 
     fun latest(): Flow<ChatMessageVM> = flowOf(storage).filter { it.isNotEmpty() }.map { it.last() }
