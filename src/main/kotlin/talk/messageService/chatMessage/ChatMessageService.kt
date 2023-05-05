85ebecc98e454893f1710bab87f2a9723005cd46
hoya <hybriddevslim@gmail.com>
2023-05-05 11:57:58 +0900

    refactor: ChatMessageService.kt 수정 - 로깅 제거

--- src/main/kotlin/talk/messageService/chatMessage/ChatMessageService.kt
+++ src/main/kotlin/talk/messageService/chatMessage/ChatMessageService.kt
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Service
 
 @Service
 class ChatMessageService {
-    private val logger = LoggerFactory.getLogger(this.javaClass)
-
     private val storage = mutableListOf<ChatMessageVM>()
 
     val sender: MutableSharedFlow<ChatMessageVM> = MutableSharedFlow()
@@ -19,11 +17,9 @@ class ChatMessageService {
                     .onEach { sender.emit(it.copy()) }
                     .collect {
                         storage.add(it)
-                        logger.debug("post messages: $storage")
                     }
 
     fun latest(): Flow<ChatMessageVM> = flowOf(storage).filter { it.isNotEmpty() }.map {
-        logger.debug("latest messages: $storage")
         it.last()
     }
 }
