baceb4af4108ce0ad812e85bd0326ced98ebfae8
sangholim <55565835+sangholim@users.noreply.github.com>
2023-05-07 17:14:51 +0900

    refactor: 메세지 발신하는 경우, chat-id에 맞는 데이터를 db에서 전달한다 (#20)

--- src/main/kotlin/talk/messageService/chatMessage/ChatMessageRepository.kt
+++ src/main/kotlin/talk/messageService/chatMessage/ChatMessageRepository.kt
@@ -1,6 +1,9 @@
 package talk.messageService.chatMessage
 
+import kotlinx.coroutines.flow.Flow
 import org.bson.types.ObjectId
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-interface ChatMessageRepository: CoroutineCrudRepository<ChatMessage, ObjectId>
+interface ChatMessageRepository: CoroutineCrudRepository<ChatMessage, ObjectId> {
+    fun findAllByChatId(chatId: String): Flow<ChatMessage>
+}
