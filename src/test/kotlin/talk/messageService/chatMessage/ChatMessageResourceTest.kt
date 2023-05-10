1dde1a7d1582cd587a6be74c4dcc4ca6b059f05c
hoya <hybriddevslim@gmail.com>
2023-05-10 20:45:49 +0900

    test: 테스트 코드 수정

--- src/test/kotlin/talk/messageService/chatMessage/ChatMessageResourceTest.kt
+++ src/test/kotlin/talk/messageService/chatMessage/ChatMessageResourceTest.kt
@@ -1,10 +1,11 @@
 package talk.messageService.chatMessage
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.reactor.asFlux
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.messaging.rsocket.RSocketRequester
 import org.springframework.messaging.rsocket.dataWithType
@@ -12,18 +13,43 @@ import org.springframework.messaging.rsocket.retrieveFlow
 import org.springframework.messaging.rsocket.retrieveFlux
 import reactor.test.StepVerifier
 import java.time.Duration
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.LinkedBlockingQueue
+
 
 @SpringBootTest
 class ChatMessageResourceTest(
         private val rSocketRequester: RSocketRequester,
-        private val repository: ChatMessageRepository
+        private val repository: ChatMessageRepository,
+        private val chatMessageStreamService: ChatMessageStreamService
 ) : BehaviorSpec({
     Given("chat 메세지 발신, 수신 stream 확인하기") {
 
         fun chatStream(id: String) = rSocketRequester.route("stream.chats.$id.message")
 
         When("chat 메세지 정상 발송한 경우") {
+            beforeTest {
+                repository.deleteAll()
+            }
+            Then("chatMessageStreamService > 1개의 메세지가 정상 수신된다") {
+                val documents: BlockingQueue<ChatMessageVM> = LinkedBlockingQueue(100)
+                val disposable = chatMessageStreamService.watchChanges("a")
+                        .asFlux()
+                        .doOnNext(documents::add)
+                        .subscribe()
+                val payload = ChatMessagePayload("a", "b")
+                chatStream("a").dataWithType(
+                        flow {
+                            emit(payload)
+                        }
+                ).retrieveFlow<Unit>().collect()
+                delay(1000)
+
+                documents.size shouldBe 1
+                disposable.dispose()
+            }
             Then("1개의 메세지가 정상 수신된다") {
+                val received = chatStream("a").retrieveFlux<ChatMessageVM>()
                 val payload = ChatMessagePayload("a", "b")
                 chatStream("a").dataWithType(
                         flow {
@@ -31,13 +57,11 @@ class ChatMessageResourceTest(
                         }
                 ).retrieveFlow<Unit>().collect()
 
-                val received = chatStream("a").retrieveFlux<ChatMessageVM>()
-                delay(1000)
-                val view = repository.findAll().first().toView()
                 StepVerifier.create(received)
-                        .expectNext(view)
+                        .expectNextCount(1)
                         .thenCancel()
                         .verify(Duration.ofSeconds(2))
+
             }
         }
     }
