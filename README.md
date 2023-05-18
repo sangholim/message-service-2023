74d416cd7cd1d54fabd5c11c91c193c331a535c6
sangholim <55565835+sangholim@users.noreply.github.com>
2023-05-19 02:50:47 +0900

    fix: rsocket-security 이전 형상 복원 (#31)
    * Revert "Revert "Update README.md" (#30)"
    
    This reverts commit f8d46b1a996ed541d554d521b953a5be778e9d53.
    
    * Revert "chore: build.gradle.kts 수정 (#29)"
    
    This reverts commit 2ec167d220d6cfb8fb3a9bef290cc0ad87599f59.
    
    * Revert "refactor: SecurityConfig.kt 수정 (#28)"
    
    This reverts commit 6e18db5f676c134eac5eab821ba02de79311331b.
    
    * Revert "refactor: rsocket security 개선 (#27)"
    
    This reverts commit 6ad1ce5d07a650cc3c6423a675dfd0d6f71c142c.
    
    * Revert "feat: rsocket-security 적용 (#26)"
    
    This reverts commit cad37b3b1e98d1c78402b1e45b6cd3cab068ddc0.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # talk-message-service-2023
-실시간 메세지 처리 서버
+- 채팅방 메세지를 처리하는 서버 입니다.
+- rsocket 의 비동기 메세지 모델 `fire and forgot` `request stream` 을 이용하여 메세지 발신 | 수신 로직을 구현
+- mongodb change stream 을 이용하여 데이터의 실시간 변경을 감지
+## framework
+- rsocket
+- reactive-mongo-client
+
--- src/test/kotlin/talk/messageService/config/WebSocketConfig.kt
+++ src/test/kotlin/talk/messageService/config/WebSocketConfig.kt
@@ -1,7 +1,5 @@
 package talk.messageService.config
 
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
 import io.rsocket.metadata.WellKnownMimeType
 import org.springframework.boot.autoconfigure.rsocket.RSocketProperties
 import org.springframework.context.annotation.Bean
@@ -10,40 +8,25 @@ import org.springframework.http.codec.cbor.Jackson2CborDecoder
 import org.springframework.http.codec.cbor.Jackson2CborEncoder
 import org.springframework.messaging.rsocket.RSocketRequester
 import org.springframework.messaging.rsocket.RSocketStrategies
-import org.springframework.security.rsocket.metadata.BearerTokenAuthenticationEncoder
-import org.springframework.security.rsocket.metadata.BearerTokenMetadata
-import org.springframework.util.MimeTypeUtils.parseMimeType
+import org.springframework.util.MimeType
 import java.net.URI
 
 @Configuration
 class WebSocketConfig(
-    private val rSocketProperties: RSocketProperties
+        private val rSocketProperties: RSocketProperties
 ) {
     private val strategies = RSocketStrategies.builder()
-        .encoders {
-            it.add(Jackson2CborEncoder())
-            it.add(BearerTokenAuthenticationEncoder())
-        }
-        .decoders {
-            it.add(Jackson2CborDecoder())
-        }
-        .build()
+            .encoders { it.add(Jackson2CborEncoder()) }
+            .decoders { it.add(Jackson2CborDecoder()) }
+            .build()
 
     @Bean
-    fun rSocketRequester(): RSocketRequester {
-        val token = Jwts.builder()
-            .signWith(JwtConfig.secretKey, SignatureAlgorithm.HS256)
-            .claim("sub", "user")
-            .compact()
+    fun rSocketRequester(): RSocketRequester =
+            RSocketRequester.builder()
+                    .rsocketStrategies(strategies)
+                    .metadataMimeType(MimeType.valueOf(WellKnownMimeType.MESSAGE_RSOCKET_COMPOSITE_METADATA.string))
+                    .websocket(
+                            URI.create("ws://${rSocketProperties.server.address.hostAddress}:${rSocketProperties.server.port}/${rSocketProperties.server.mappingPath}")
+                    )
 
-        return RSocketRequester.builder()
-            .rsocketStrategies(strategies)
-            .setupMetadata(
-                BearerTokenMetadata(token),
-                parseMimeType(WellKnownMimeType.MESSAGE_RSOCKET_AUTHENTICATION.string)
-            )
-            .websocket(
-                URI.create("ws://${rSocketProperties.server.address.hostAddress}:${rSocketProperties.server.port}/${rSocketProperties.server.mappingPath}")
-            )
-    }
 }
